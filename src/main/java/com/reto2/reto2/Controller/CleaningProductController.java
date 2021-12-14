9e6cdf6fc83d3f4529b5111cee66926cecfa7176
andres.fonseca <andrespipefon@hotmail.com>
2021-12-14 15:47:45 -0500

    Add @CrossOrigin(origins = "*")

--- src/main/java/com/reto2/reto2/Controller/CleaningProductController.java
+++ src/main/java/com/reto2/reto2/Controller/CleaningProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/cleaningprod")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class CleaningProductController {
     @Autowired
     private CleaningProductService cleaningProductService;
--- src/main/java/com/reto2/reto2/Controller/UserController.java
+++ src/main/java/com/reto2/reto2/Controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/user")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserService userService;
