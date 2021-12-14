2bfb4ba89ccd4d303fdac5e99dd10c70c9cf1b62
Andres Felipe Fonseca Mosquera <andrespipefon@hotmail.com>
2021-12-14 14:55:50 -0500

    Add @CrossOrigin

--- src/main/java/com/reto2/reto2/Controller/CleaningProductController.java
+++ src/main/java/com/reto2/reto2/Controller/CleaningProductController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/cleaningprod")
+@CrossOrigin("*")
 public class CleaningProductController {
     @Autowired
     private CleaningProductService cleaningProductService;
