9be5a7e427334648dc1894834bf27a71adf747ef
Andres Fonseca <andrespipefon@hotmail.com>
2021-12-14 21:12:53 -0500

    Changes

--- front/assets/js/register.js
+++ front/assets/js/register.js
@@ -1,38 +1,35 @@
-function registerUser(){
-    let email = $("#email").val(),
-        password = $("#password").val(),
-        repassword = $("#repassword").val();
+function postUserData(){
+    var email =  $("#email").val();
+    var password = $("#password").val();
+    var repassword = $("#repassword").val();
     if(validateEmail(email)){
-        if(validarPassword(password,repassword)){
-            let data = {
+        if(validarPassword(password, repassword)){
+            var data = {
                 identification : $("#identification").val(),
-                name : $("#name").val(),
-                lastname : $("#lastname").val(),
+                name : $("#name").val() + " " + $("#lastname").val(),        
                 address : $("#address").val(),
                 cellPhone : $("#cellPhone").val(),
                 email : email,
-                password : password,
-                repassword : repassword,
+                password : password,        
                 zone : $("#zone").val(),
                 type : $("#type").val()
             }
-
-            let dataToSend = JSON.stringify(data);
             $.ajax({
-                url: "http://158.101.116.78:8080/api/user/new",
-                data: dataToSend,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: JSON.stringify(data),
+                url: "http://localhost:8080/api/user/new",
                 type: "POST",
-                dataType: "application/JSON",
-                contentType: "json",
                 success: function(response){
+                    console.log(response);
                     alert("Usuario creado correctamente!");
                 },
                 error: function(xhr, status){
-                    console.log(status);
+                    console.log(xhr);
                 }
-            });
-        };        
-    };                
+            }); 
+        }
+    }                 
 }
 
 function validarPassword(p1, p2){
@@ -44,4 +41,4 @@ function validarPassword(p1, p2){
 function validateEmail(email) {
     var emailReg = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return emailReg.test(email);
-  }
+}
--- src/main/java/com/reto2/reto2/Controller/CleaningProductController.java
+++ src/main/java/com/reto2/reto2/Controller/CleaningProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/cleaningprod")
+@RequestMapping("/api/cleaningprod")
 @CrossOrigin(origins = "*")
 public class CleaningProductController {
     @Autowired
